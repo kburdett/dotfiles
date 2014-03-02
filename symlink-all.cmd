995537075901d7fddfe294e20176e0e7db67597f
Kevin Burdett <kevin@kevinburdett.com>
2014-03-02 08:53:59 -0500

    Added gitconfig and gitignore dotfiles and cleaned out some temp files

--- symlink-all.cmd
+++ symlink-all.cmd
@@ -3,7 +3,11 @@ set DIR=%~dp0
 del %USERPROFILE%\_vimrc
 del %USERPROFILE%\_gvimrc
 rmdir %USERPROFILE%\vimfiles
+del %USERPROFILE%\.gitconfig
+del %USERPROFILE%\.gitignore
 
 mklink  %USERPROFILE%\_vimrc %DIR%vimrc
 mklink  %USERPROFILE%\_gvimrc %DIR%gvimrc
 mklink /d %USERPROFILE%\vimfiles %DIR%vim
+mklink  %USERPROFILE%\.gitconfig %DIR%gitconfig
+mklink  %USERPROFILE%\.gitignore %DIR%gitignore
