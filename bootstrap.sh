ddd4d5fc691f7a7d61f33d4feafd709fb372da80
Kevin Burdett <kevin@kevinburdett.com>
2014-03-09 20:44:51 -0400

    Removing unused code from bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 dir=~/dotfiles
-olddir=~/dotfiles_old             # old dotfiles backup directory
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $dir/*.symlink; do
     echo "Creating symlink to $file in home directory."
     filename=${file##*/}
