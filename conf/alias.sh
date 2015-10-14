b90158f38be03c902c67f2f7456ec7d025835d6b
Kevin Burdett <kevin@kevinburdett.com>
2015-10-14 14:32:35 -0400

    Restructure to share alias and path conf betewen ZSH and bash

---
+++ conf/alias.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# ls aliases
+case $(uname -s) in
+    Darwin|FreeBSD|OpenBSD)
+        # BSD
+        alias ls='ls -Gph'
+        alias ll='ls -Gphl'
+        alias la='ls -Gphal'
+        ;;
+    *)
+        # GNU
+        alias ls='ls -ph --color'
+        alias ll='ls -phl --color'
+        alias la='ls -phal --color'
+        ;;
+esac
+
+# cd aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias .......='cd ../../../../../..'
+alias ........='cd ../../../../../../..'
+alias .........='cd ../../../../../../../..'
+alias ..........='cd ../../../../../../../../..'
+alias .2='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+alias .6='cd ../../../../../..'
+alias .7='cd ../../../../../../..'
+alias .8='cd ../../../../../../../..'
+alias .9='cd ../../../../../../../../..'
+alias .10='cd ../../../../../../../../../..'
+alias .11='cd ../../../../../../../../../../..'
+
+# misc aliases
+alias path='echo -e ${PATH//:/\\n}'
+alias ping='ping -c 5'
+alias ax='chmod a+x'
+alias hig='history | grep'
+alias mkdir='mkdir -pv'
+alias dh='dirs -v'
