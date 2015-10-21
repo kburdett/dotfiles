d949e48688e37f4a12f22c4806abc1052f77aff1
Kevin Burdett <kevin@kevinburdett.com>
2015-10-21 11:08:08 -0400

    Add comment sourcing BC4 configuration details

--- util/git-config-system-osx.sh
+++ util/git-config-system-osx.sh
@@ -7,6 +7,7 @@ echo "Configuring autoclrf..."
 git config --system core.autocrlf input
 
 echo "Configuring Beyond Compare 4"
+# NOTE: using BC3 options per docs http://www.scootersoftware.com/support.php?zz=kb_vcs_osx
 git config --system diff.tool bc3
 git config --system merge.tool bc3
 git config --system mergetool.bc3 trustExitCode true
