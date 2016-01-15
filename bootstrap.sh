ac5a753544d094f53d85109f7e040eed4f6349d5
Kevin Burdett <kevin@kevinburdett.com>
2016-01-15 17:33:37 -0500

    Revise OS check for homebrew install

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,18 +7,22 @@ for file in $dir/*.symlink; do
     ln -sf $file ~/.${filename%.symlink}
 done
 
-# install homebrew
-if [[ $OSTYPE == darwin* && ! $(command -v brew 2>/dev/null) ]]; then
-    echo "Mac without homebrew?!?! Fixing..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-    echo "Homebrew already installed"
-fi
+# Homebrew setup, if OSX
+if [[ $OSTYPE == darwin* ]]; then
 
-# install brew bundle
-if [[ ! $(brew bundle check) ]]; then
-    echo "Found missing packages, installing Homebrew bundle"
-    brew bundle
-else
-    echo "Homebrew bundle reports all packages installed"
+    # install homebrew
+    if [[ ! $(command -v brew 2>/dev/null) ]]; then
+        echo "Mac without homebrew?!?! Fixing..."
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    else
+        echo "Homebrew already installed"
+
+    # install brew bundle
+    if [[ ! $(brew bundle check) ]]; then
+        echo "Found missing packages, installing Homebrew bundle"
+        brew bundle
+    else
+        echo "Homebrew bundle reports all packages installed"
+    fi
+
 fi
