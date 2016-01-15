ec574be7f24ef6c225a11a9690ac4ff3c941de23
Kevin Burdett <kevin@kevinburdett.com>
2016-01-15 17:44:00 -0500

    Add -n flag to LN to avoid following directory symlinks
    Previously, I was using -h for this, but it wasn't portable to GNU
    systems. The -n flag will work on either.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ dir=~/dotfiles
 for file in $dir/*.symlink; do
     echo "Creating symlink to $file in home directory."
     filename=${file##*/}
-    ln -sf $file ~/.${filename%.symlink}
+    ln -sfn $file ~/.${filename%.symlink}
 done
 
 # Homebrew setup, if OSX
