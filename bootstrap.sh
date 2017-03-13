b41d523720d3c58355a11eeb103fa7742db24533
Kevin Burdett <kevin@kevinburdett.com>
2017-03-13 16:35:14 -0400

    Remove NVM

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,15 +8,6 @@ for file in $dir/*.symlink; do
     ln -sfn $file ~/.${filename%.symlink}
 done
 
-if [[ -z ~/.nvm/nvm.sh ]]; then
-    echo Installing NVM
-    current=$(pwd)
-    git clone https://github.com/creationix/nvm.git ~/.nvm
-    cd ~/.nvm
-    git checkout $(git describe --abbrev=0 --tags)
-    cd $current
-fi
-
 if [[ $OSTYPE == darwin* ]]; then
     echo Symlinking OSX gitconfig
     ln -sfn ~/dotfiles/conf/gitconfig.osx ~/.gitconfig.os
