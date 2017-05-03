d400039490e757bd5f741e4d171335a0959b2715
Kevin Burdett <kevin.burdett@pps.io>
2017-05-03 18:39:22 -0400

    Use stow's -v for logging

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,9 +6,6 @@ echo Importing ENV configuration
 # shellcheck source=/dev/null
 source "$dir/conf/env.sh"
 
-echo Stowing home directory files
-stow -Rt "$HOME" home
-
-echo Stowing XDG config files
-stow -Rt "$XDG_CONFIG_HOME" xdg
+stow -Rvt "$HOME" home 
+stow -Rvt "$XDG_CONFIG_HOME" xdg
 
