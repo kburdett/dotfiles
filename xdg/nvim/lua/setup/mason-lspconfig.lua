28bccc1904f4a8b558b532739ebbc32bb8ba5981
Kevin Burdett <kevin.burdett@pps.io>
2023-03-07 17:58:04 -0500

    Add default LSP installers for Mason

--- xdg/nvim/lua/setup/mason-lspconfig.lua
+++ xdg/nvim/lua/setup/mason-lspconfig.lua
@@ -1,3 +1,18 @@
 
-require('mason-lspconfig').setup()
+require('mason-lspconfig').setup({
+
+    ensure_installed = {
+        'bashls', -- bash
+        'dockerls', -- dockerfile
+        'docker_compose_language_service', -- docker compose
+        'json-lsp', -- json
+        'lemminx', -- xml
+        'lua_ls', -- lua
+        'marksman', -- markdown
+        'powershell_es', -- powershell
+        'tsserver', -- javascript
+        'yamlls', -- yaml
+    },
+
+})
 
