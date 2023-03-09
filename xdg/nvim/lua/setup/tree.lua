994d65aff1793d9e53da146ae2dc6bbb6978a760
Kevin Burdett <kevin.burdett@pps.io>
2023-03-09 15:59:24 -0500

    Add mappings for opening tree

--- xdg/nvim/lua/setup/tree.lua
+++ xdg/nvim/lua/setup/tree.lua
@@ -1,1 +1,5 @@
 require('nvim-tree').setup()
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<C-t>', '<cmd>NvimTreeFocus<cr>', opts)
+vim.keymap.set('n', '<C-f>', '<cmd>NvimTreeFindFile<cr>', opts)
