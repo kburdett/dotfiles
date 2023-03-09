a8be8afe9860692fe6ed0ee6abc7bd49082cac9e
Kevin Burdett <kevin.burdett@pps.io>
2023-03-09 15:49:31 -0500

    Conditionally load plugins based on vscode hosting

--- xdg/nvim/lua/lazy_setup.lua
+++ xdg/nvim/lua/lazy_setup.lua
@@ -4,25 +4,28 @@ local get_setup = function(name)
     end
 end
 
+local isNative = not vim.g.vscode
+
 return {
     -- colorschemes
     {
         'navarasu/onedark.nvim',
         lazy = false,
         priority = 1000,
+        cond = isNative,
         config = get_setup('onedark'),
     },
 
     -- core
     'kburdett/vim-nuuid',
     { 'echasnovski/mini.nvim',             config = get_setup('mini') },
-    { 'lewis6991/gitsigns.nvim',           config = get_setup('gitsigns') },
+    { 'lewis6991/gitsigns.nvim',           cond = isNative, config = get_setup('gitsigns') },
 
     -- ui
-    { 'nvim-lualine/lualine.nvim',         config = get_setup('lualine') },
-    { 'kdheepak/tabline.nvim',             config = get_setup('tabline') },
-    { 'nvim-tree/nvim-web-devicons' },
-    { 'nvim-tree/nvim-tree.lua',           config = get_setup('tree') },
+    { 'nvim-tree/nvim-web-devicons',       cond = isNative },
+    { 'nvim-lualine/lualine.nvim',         cond = isNative, config = get_setup('lualine') },
+    { 'kdheepak/tabline.nvim',             cond = isNative, config = get_setup('tabline') },
+    { 'nvim-tree/nvim-tree.lua',           cond = isNative, config = get_setup('tree') },
 
     -- filetypes
     { 'chrisbra/csv.vim',                  ft = 'csv' },
@@ -31,18 +34,19 @@ return {
     { 'ekalinin/Dockerfile.vim',           ft = 'Dockerfile' },
 
     -- lsp
-    { 'williamboman/mason.nvim',           config = get_setup('mason') },
-    { 'williamboman/mason-lspconfig.nvim', config = get_setup('mason-lspconfig') },
-    { 'neovim/nvim-lspconfig',             config = get_setup('lspconfig') },
+    { 'williamboman/mason.nvim',           cond = isNative, config = get_setup('mason') },
+    { 'williamboman/mason-lspconfig.nvim', cond = isNative, config = get_setup('mason-lspconfig') },
+    { 'neovim/nvim-lspconfig',             cond = isNative, config = get_setup('lspconfig') },
 
     -- snippets
-    { 'L3MON4D3/LuaSnip' },
-    { 'rafamadriz/friendly-snippets' },
+    { 'L3MON4D3/LuaSnip', cond = isNative },
+    { 'rafamadriz/friendly-snippets', cond = isNative },
 
     -- completion
     {
         'hrsh7th/nvim-cmp',
         event = 'InsertEnter',
+        cond = isNative,
         config = get_setup('cmp'),
         dependencies = {
             'hrsh7th/cmp-nvim-lsp',
