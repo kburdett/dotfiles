92d95e2ded751edcbe3f9adc58d839a47d62b592
Kevin Burdett <kevin.burdett@pps.io>
2023-03-09 17:34:22 -0500

    Move tree back to `n` because it's more familiar

--- xdg/nvim/lua/setup/tree.lua
+++ xdg/nvim/lua/setup/tree.lua
@@ -1,5 +1,5 @@
 require('nvim-tree').setup()
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<leader>d', '<cmd>NvimTreeFocus<cr>', opts)
+vim.keymap.set('n', '<leader>n', '<cmd>NvimTreeFocus<cr>', opts)
 vim.keymap.set('n', '<leader>f', '<cmd>NvimTreeFindFile<cr>', opts)
