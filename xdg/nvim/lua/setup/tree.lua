41b46068b09fb208d1075c5994d2bdab70ef8956
Kevin Burdett <kevin.burdett@pps.io>
2023-03-09 17:22:03 -0500

    Move tree mappings to leader

--- xdg/nvim/lua/setup/tree.lua
+++ xdg/nvim/lua/setup/tree.lua
@@ -1,5 +1,5 @@
 require('nvim-tree').setup()
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<C-t>', '<cmd>NvimTreeFocus<cr>', opts)
-vim.keymap.set('n', '<C-f>', '<cmd>NvimTreeFindFile<cr>', opts)
+vim.keymap.set('n', '<leader>d', '<cmd>NvimTreeFocus<cr>', opts)
+vim.keymap.set('n', '<leader>f', '<cmd>NvimTreeFindFile<cr>', opts)
