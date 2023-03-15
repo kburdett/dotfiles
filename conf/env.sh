e70a6ea6d05c35e2bdd578fee8485705ff14017c
Kevin Burdett <kevin@kevinburdett.com>
2023-03-15 02:11:57 -0400

    Add XDG paths for linux environment

--- conf/env.sh
+++ conf/env.sh
@@ -1,6 +1,6 @@
 
 # Set the homebrew token, not checked into dotfiles for obvious reasons :)
-if [[ -s "$HOME/.homebrew_api_token" ]]; then 
+if [[ -s "$HOME/.homebrew_api_token" ]]; then
     HOMEBREW_API_TOKEN=$(cat "$HOME/.homebrew_api_token")
     export HOMEBREW_API_TOKEN
 fi
@@ -9,17 +9,26 @@ fi
 export EDITOR='nvim'
 export VISUAL='nvim'
 
+
 # Configure XDG paths
 case $OSTYPE in
     darwin*)
         export XDG_CONFIG_HOME="$HOME/Library/Preferences"
         export XDG_DATA_HOME="$HOME/Library"
-        export XDG_CACHE_HOME="$HOME/Library/Caches" 
+        export XDG_CACHE_HOME="$HOME/Library/Caches"
         ;;
     msys|cygwin)
         export XDG_CONFIG_HOME="$HOME/AppData/Roaming"
         export XDG_DATA_HOME="$HOME/AppData/Local"
-        export XDG_CACHE_HOME="$HOME/AppData/Local/Temp" 
+        export XDG_CACHE_HOME="$HOME/AppData/Local/Temp"
+        ;;
+    *)
+        export XDG_CONFIG_HOME="$HOME/.config"
+        export XDG_DATA_HOME="$HOME/.local/share"
+        export XDG_CACHE_HOME="$HOME/.cache"
         ;;
 esac
 
+
+# Add global NPM binaries to the path
+export PATH=~/$XDG_DATA_HOME/npm/bin:$PATH
