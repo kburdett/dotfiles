41e7550fbc222642cfb35926efce4fccfdb7aa74
Kevin Burdett <kevin@kevinburdett.com>
2023-03-15 11:52:36 -0400

    Add powershell profile.ps1
    This profile exports XDG dirs for windows as well :D
    We should be able to ignore legacy gitconfig with this

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,28 +1,35 @@
-$XdgConfigDir = $Env:LOCALAPPDATA
-$HomeDir = $Env:USERPROFILE
+$DotfilesDir = Split-Path -parent $PSCommandPath
+
+# configure the powershell profile
+Write-Host "Deploying powershell profile"
+$DotfilesProfile = Join-Path -Path $DotfilesDir -ChildPath "win" -AdditionalChildPath "profile.ps1"
+Write-Host "Symlinking gitconfig ""$Profile.CurrentUserAllHosts"" --> ""$DotfilesProfile"""
+New-Item -ItemType SymbolicLink -Path $Profile.CurrentUserAllHosts -Target $DotfilesProfile -Force
+
+# source the profile for XDG dirs
+& $Profile.CurrentUserAllHosts
 
 function Link-All {
 
     param (
-        [string]$Source,
-        [string]$Dest
+        [string]$TargetPath,
+        [string]$LinkPath
     )
 
-    foreach ($Child in Get-ChildItem -Path $Source -Name -Force) {
-        $SourceFqn = Join-Path -Path $Source -ChildPath $Child
-        $TargetFqn = Join-Path -Path $Dest -ChildPath $Child
+    foreach ($Child in Get-ChildItem -Path $TargetPath -Name -Force) {
+        $LinkFqn = Join-Path -Path $LinkPath -ChildPath $Child
+        $TargetFqn = Join-Path -Path $TargetPath -ChildPath $Child
 
-        Write-Host "Symlinking $Child from ""$SourceFqn"" to ""$TargetFqn"""
-        New-Item -ItemType SymbolicLink -Path $SourceFqn -Target $TargetFqn -Force
+        Write-Host "Symlinking $Child ""$LinkFqn"" to ""$TargetFqn"""
+        New-Item -ItemType SymbolicLink -Path $LinkFqn -Target $TargetFqn -Force
     }
 }
 
-$DotfilesDir = Split-Path -parent $PSCommandPath
 
-Write-Host "Deploying XDG configurations to ""$XdgConfigDir"""
+Write-Host "Deploying XDG configurations to ""$Env:XDG_CONFIG_HOME"""
 $XdgDotfilesDir = Join-Path -Path $DotfilesDir -ChildPath "xdg"
-Link-All -Source $XdgDotfilesDir -Dest $XdgConfigDir
+Link-All -TargetPath $XdgDotfilesDir -LinkPath $Env:XDG_CONFIG_HOME
 
-Write-Host "Deploying dotfiles to ""$HomeDir"""
+Write-Host "Deploying dotfiles to ""$Env:USERPROFILE"""
 $HomeDotfilesDir = Join-Path -Path $DotfilesDir -ChildPath "home"
-Link-All -Source $HomeDotfilesDir -Dest $HomeDir
+Link-All -TargetPath $HomeDotfilesDir -LinkPath $Env:USERPROFILE
