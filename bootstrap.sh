f3b4d51d257e16b3526e968e2ae26ff478983e30
Kevin Burdett <kevin@kevinburdett.com>
2023-03-15 02:10:40 -0400

    Add linux git config

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,18 @@ echo Importing ENV configuration
 source "$dir/conf/env.sh"
 
 stow -Rvt "$HOME" home 
+mkdir -p "$XDG_CONFIG_HOME"
 stow -Rvt "$XDG_CONFIG_HOME" xdg
 
-ln -s $dir/xdg/git/config.osx $dir/xdg/git/config.os
-
+# Configure OS specific git config
+case $OSTYPE in
+    darwin*)
+        ln -sf $dir/xdg/git/config.osx $dir/xdg/git/config.os
+        ;;
+    msys|cygwin)
+        ln -sf $dir/xdg/git/config.win $dir/xdg/git/config.os
+        ;;
+    *)
+        ln -sf $dir/xdg/git/config.linux $dir/xdg/git/config.os
+        ;;
+esac
