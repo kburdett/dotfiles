bb91c5a61d1a8f3c941b1df64b5fe19051c63263
Kevin Burdett <kevin@kevinburdett.com>
2023-03-15 11:59:28 -0400

    Add editor config to powershell profile

--- win/profile.ps1
+++ win/profile.ps1
@@ -1,3 +1,8 @@
+# editor setup
+$Env:EDITOR = "nvim"
+$Env:VISUAL = "nvim"
+
+# XDG directories setup
 $Env:XDG_CONFIG_HOME = "$Env:APPDATA"
 $Env:XDG_DATA_HOME = "$Env:LOCALAPPDATA"
 $Env:XDG_CACHE_HOME = "$Env:TEMP"
