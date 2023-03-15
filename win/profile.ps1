d28df103fd9e5c195c75ab625352e818f2e7b86c
Kevin Burdett <kevin@kevinburdett.com>
2023-03-15 12:03:17 -0400

    Add vi mode for PSReadline

--- win/profile.ps1
+++ win/profile.ps1
@@ -6,3 +6,6 @@ $Env:VISUAL = "nvim"
 $Env:XDG_CONFIG_HOME = "$Env:APPDATA"
 $Env:XDG_DATA_HOME = "$Env:LOCALAPPDATA"
 $Env:XDG_CACHE_HOME = "$Env:TEMP"
+
+# Set vi mode for readline
+Set-PSReadlineOption -EditMode vi
