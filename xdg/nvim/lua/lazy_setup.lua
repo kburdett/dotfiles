76800396c35582b89ed90a11aa8fb1df76a6400d
Kevin Burdett <kevin.burdett@pps.io>
2023-03-23 11:37:38 -0400

    Add trouble plugin

--- xdg/nvim/lua/lazy_setup.lua
+++ xdg/nvim/lua/lazy_setup.lua
@@ -17,16 +17,16 @@ return {
     },
 
     -- core
-    'kburdett/vim-nuuid',
     { 'echasnovski/mini.nvim',             config = get_setup('mini') },
-    { 'lewis6991/gitsigns.nvim',           cond = isNative, config = get_setup('gitsigns') },
+    { 'lewis6991/gitsigns.nvim',           cond = isNative,           config = get_setup('gitsigns') },
 
     -- ui
     { 'nvim-tree/nvim-web-devicons',       cond = isNative },
-    { 'nvim-lualine/lualine.nvim',         cond = isNative, config = get_setup('lualine') },
-    { 'kdheepak/tabline.nvim',             cond = isNative, config = get_setup('tabline') },
-    { 'nvim-tree/nvim-tree.lua',           cond = isNative, config = get_setup('tree') },
-    { 'akinsho/toggleterm.nvim',           cond = isNative, config = get_setup('toggleterm') },
+    { 'nvim-lualine/lualine.nvim',         cond = isNative,           config = get_setup('lualine') },
+    { 'kdheepak/tabline.nvim',             cond = isNative,           config = get_setup('tabline') },
+    { 'nvim-tree/nvim-tree.lua',           cond = isNative,           config = get_setup('tree') },
+    { 'akinsho/toggleterm.nvim',           cond = isNative,           config = get_setup('toggleterm') },
+    { 'folke/trouble.nvim',                cond = isNative,           config = get_setup('trouble') },
 
     -- filetypes
     { 'chrisbra/csv.vim',                  ft = 'csv' },
@@ -35,13 +35,13 @@ return {
     { 'ekalinin/Dockerfile.vim',           ft = 'Dockerfile' },
 
     -- lsp
-    { 'williamboman/mason.nvim',           cond = isNative, config = get_setup('mason') },
-    { 'williamboman/mason-lspconfig.nvim', cond = isNative, config = get_setup('mason-lspconfig') },
-    { 'neovim/nvim-lspconfig',             cond = isNative, config = get_setup('lspconfig') },
+    { 'williamboman/mason.nvim',           cond = isNative,           config = get_setup('mason') },
+    { 'williamboman/mason-lspconfig.nvim', cond = isNative,           config = get_setup('mason-lspconfig') },
+    { 'neovim/nvim-lspconfig',             cond = isNative,           config = get_setup('lspconfig') },
 
     -- snippets
-    { 'L3MON4D3/LuaSnip', cond = isNative },
-    { 'rafamadriz/friendly-snippets', cond = isNative },
+    { 'L3MON4D3/LuaSnip',                  cond = isNative },
+    { 'rafamadriz/friendly-snippets',      cond = isNative },
 
     -- completion
     {
